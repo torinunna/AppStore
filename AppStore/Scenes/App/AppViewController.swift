ef097fd19d60d65852a78248c427fd49e22c4b90
torinunna <torinunna9588@gmail.com>
2022-12-06 23:33:19 +0900

    create AppVC and embed navigaionController

---
+++ AppStore/Scenes/App/AppViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AppViewController.swift
+//  AppStore
+//
+//  Created by YUJIN KWON on 2022/12/06.
+//
+import SnapKit
+import UIKit
+
+final class AppViewController: UIViewController {
+
+}
