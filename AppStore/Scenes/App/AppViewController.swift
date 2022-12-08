c1a53bfa8b8f2ca428235180b303712bb8e8ded7
torinunna <torinunna9588@gmail.com>
2022-12-08 23:18:16 +0900

    add scrollView, contentView, stackView to AppVC

--- AppStore/Scenes/App/AppViewController.swift
+++ AppStore/Scenes/App/AppViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 final class AppViewController: UIViewController {
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        
+        let featureSectionView = UIView()
+        let rankingFeatureSectionView = UIView()
+        let exchangeCodeButtonView = UIView()
+        
+        featureSectionView.backgroundColor = .red
+        rankingFeatureSectionView.backgroundColor = .blue
+        exchangeCodeButtonView.backgroundColor = .yellow
+        
+        [featureSectionView, rankingFeatureSectionView, exchangeCodeButtonView].forEach {
+            $0.snp.makeConstraints { $0.height.equalTo(500.0) }
+            stackView.addArrangedSubview($0)
+        }
+        
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpNavigationController()
+        setUpLayout()
     }
 
 }
@@ -23,5 +49,26 @@ private extension AppViewController {
         navigationItem.title = "앱"
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func setUpLayout() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
